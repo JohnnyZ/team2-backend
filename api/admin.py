e3f32a950c4b726e114dfe6987a38966f828a645
Johnny Z <JohnZadroga@gmail.com>
2015-03-25 17:11:16 -0400

    resyncing model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from api.models import appUser MeditationSession ExcerciseSession
+
 # Register your models here.
+admin.site.register(appUser)
+admin.site.register(MeditationSession)
+admin.site.register(ExcerciseSession)
