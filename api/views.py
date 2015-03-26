6de95a0136711f6cf15d2577e865524cff76f574
Johnny Z <JohnZadroga@gmail.com>
2015-03-25 23:11:41 -0400

    MediationSessionSerializer restapi / url configuration

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from serializers import MeditationSerializers
+from models import MeditationSession
 
-# Create your views here.
+class MeditationSessionList(generics.ListCreateAPIView):
+	queryset = MeditationSession.objects.all()
+	serializer_class = ZipSerializer
+
+# All update delete handling
+class MeditationSessionDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = MeditationSession.objects.all()
+	serializer_class = ZipSerializer
