8f43b57f28055c96f2a019491d123c94e7d90e2e
Johnny Z <JohnZadroga@gmail.com>
2015-03-26 00:54:29 -0400

    updates to api

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+# TastyPie
+from tastypie.api import Api
 from api.api import MeditationResource
+v1_api = Api()
+v1_api.register(MeditationResource())
 
 meditation_resource = MeditationResource
 
@@ -13,7 +17,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^backend/', include('api.urls')),
-    url(r'^api/', include(meditation_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')), 
 )
