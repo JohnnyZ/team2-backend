1626fecfad13bcfa56625b2a9edb5ad0e1c26615
Johnny Z <JohnZadroga@gmail.com>
2015-03-26 00:46:25 -0400

    update api.py

--- api/api.py
+++ api/api.py
@@ -4,4 +4,4 @@ from api.models import *
 class MeditationResource(ModelResource):
 	class Meta:
 		queryset = MeditationSession.objects.all()
-		resource_name = 'meditationsession'
+		resource_name = 'medsession'
