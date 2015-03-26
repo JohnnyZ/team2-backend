35aacd645b093bb73e7104f11af1ab77cb5a79b4
Johnny Z <JohnZadroga@gmail.com>
2015-03-26 01:02:18 -0400

    updating api

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
 from tastypie.resources import ModelResource
+from tastypie.serializers import Serializer
 from api.models import * 
+
 
 class MeditationResource(ModelResource):
 	class Meta:
 		queryset = MeditationSession.objects.all()
 		resource_name = 'medsession'
+		serializer = Serializer(formats=['json'])
--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views 
 
 urlpatterns = patterns('', 
-		url(r'^api/$', views.MeditationSessionList.as_view()),
-		url(r'^api/(?P<pk>[0-9]+)/$', views.MeditationSessionDetail.as_view()),
+		#url(r'^api/$', views.MeditationSessionList.as_view()),
+		url(r'^api22/(?P<pk>[0-9]+)/$', views.MeditationSessionDetail.as_view()),
 		)
 
-urlpatters = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
+#urlpatters = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
