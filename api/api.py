a735daf42817095f056ae7ab9f3d0c70cf7eff14
Johnny Z <JohnZadroga@gmail.com>
2015-03-26 01:23:22 -0400

    remove field names

--- api/api.py
+++ api/api.py
@@ -17,6 +17,7 @@ class UserResource(ModelResource):
 	class Meta:
 		queryset = User.objects.all()
 		resource_name = 'user'
+		excludes = ['email', 'password', 'is_staff', 'is_superuser']
 
 class appUserResource(ModelResource):
 	class Meta:
