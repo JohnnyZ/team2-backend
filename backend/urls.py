f2b6cd3d7a2f0b130cca2aabef53347c5ac7b1d8
Johnny Z <JohnZadroga@gmail.com>
2015-03-25 23:17:21 -0400

    edit urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'backend.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^backend/', include('api.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')), 
-    url(r'^api/', include(api.urls)),
 )
