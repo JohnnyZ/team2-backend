fb7a6dd5b9ea2626ec6f365a7e4bd15d871fc0bf
Johnny Z <JohnZadroga@gmail.com>
2015-03-25 22:10:38 -0400

    add rest_framework

--- gpush.sh
+++ gpush.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 pip freeze > requirements.txt
 sudo git add -A
-echo 'Enter the git commit message for heroku and backend..'
+echo 'Enter the git commit message for backend.. ONLY'
 read gitcommit
 sudo git commit -m "$gitcommit"
 sudo git push backend
-sudo git push heroku
