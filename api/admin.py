a8240b11ce119dfcf02bcdd896035daef827bee9
Johnny Z <JohnZadroga@gmail.com>
2015-03-30 18:25:07 -0400

    add appUserInline to show in admin

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 from api.models import appUser, MeditationSession, ExerciseSession
 
+# Add appUser to user 
+admin.site.unregister(User)
+
+class appUserInline(admin.StackedInline):
+    model = appUser
+
+class appUserAdmin(UserAdmin):
+    inlines = [ appUserInline, ]
 
 
+
+"""
 class appUserAdmin(admin.ModelAdmin):
-	
 	list_display = ('user_id_display', 'user', 'start_date', 'mediation_time', 'exercise_day_of_week',
 			'exercise_time', 'created_at', 'updated_at')
 	fields = ['user', 'start_date', 'mediation_time', 'exercise_day_of_week',
@@ -15,6 +26,7 @@ class appUserAdmin(admin.ModelAdmin):
 	def user_id_display(self, obj):
 		return obj.user_id
 	user_id_display.short_description = 'User ID'
+"""
 
 class MeditationSessionAdmin(admin.ModelAdmin):
 	list_display = ('id', 'meditation_id', 'user_id_display', 'user', 'percent_completed', 'created_at', 'updated_at')
@@ -36,7 +48,8 @@ class ExerciseSessionAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(appUser, appUserAdmin)
+#admin.site.register(appUser, appUserAdmin)
+admin.site.register(User, appUserAdmin)
 admin.site.register(MeditationSession, MeditationSessionAdmin)
 admin.site.register(ExerciseSession, ExerciseSessionAdmin)
 
