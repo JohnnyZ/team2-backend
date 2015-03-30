847fe8a4c9796059656c33d67c95be6ec6ea9552
Johnny Z <JohnZadroga@gmail.com>
2015-03-30 19:24:52 -0400

    admin.py appuseradmin

--- api/admin.py
+++ api/admin.py
@@ -5,9 +5,9 @@ from api.models import appUser, MeditationSession, ExerciseSession
 
 """
 class appUserAdmin(admin.ModelAdmin):
-	list_display = ('user_id_display', 'user', 'start_date', 'mediation_time', 'exercise_day_of_week',
+	list_display = ('user_id_display', 'user', 'start_date', 'meditation_time', 'exercise_day_of_week',
 			'exercise_time', 'created_at', 'updated_at')
-	fields = ['user', 'start_date', 'mediation_time', 'exercise_day_of_week',
+	fields = ['user', 'start_date', 'meditation_time', 'exercise_day_of_week',
 			'exercise_time', 'created_at', 'updated_at']
 	readonly_fields = ('created_at', 'updated_at')
 
@@ -40,16 +40,40 @@ class appUserInline(admin.StackedInline):
 
 class appUserAdmin(UserAdmin):
     inlines = [ appUserInline, ]
-    list_display = ('id', 'username', 'start_date', )
+    list_display = ('id', 'username', 'start_date', 'meditation_time', 'exercise_day_of_week', 'exercise_time',   )
 
-    def start_date(self, obj):
+	def start_date(self, obj):
     	try:
     		start_date = obj.appuser.start_date
     		return start_date
     	except:
     		return ""
+	start_date.short_description = 'Start Date'
 
-    start_date.short_description = 'Start Date'
+    def meditation_time(self, obj):
+    	try:
+    		meditation_time = obj.appuser.meditation_time
+    		return meditation_time
+    	except:
+    		return ""
+	meditation_time.short_description = 'Med Time'
+
+    def exercise_day_of_week(self, obj):
+    	try:
+    		exercise_day_of_week = obj.appuser.exercise_day_of_week
+    		return exercise_day_of_week
+    	except:
+    		return ""
+	exercise_day_of_week.short_description = 'Exercise Day'
+
+    def exercise_time(self, obj):
+    	try:
+    		exercise_time = obj.appuser.exercise_time
+    		return exercise_time
+    	except:
+    		return ""
+	exercise_time.short_description = 'Exercise Time'
+
 
 ## Register models ##
 
