2d76ff145aea81c0cf6d49126be917e5681afddf
Johnny Z <JohnZadroga@gmail.com>
2015-03-30 21:12:32 -0400

    Add login and logout for user

--- api/api.py
+++ api/api.py
@@ -21,27 +21,70 @@ class UserResource(ModelResource):
 		filtering = {
 			'username': ALL,
 		}
+	def override_urls(self):
+		return [
+			url(r"^(?P<resource_name>%s)/login%s$" %
+				(self._meta.resource_name, trailing_slash()),
+				self.wrap_view('login'), name="api_login"),
+			url(r'^(?P<resource_name>%s)/logout%s$' %
+				(self._meta.resource_name, trailing_slash()),
+				self.wrap_view('logout'), name='api_logout'),
+		]
+
+	def login(self, request, **kwargs):
+		self.method_check(request, allowed=['post'])
+
+		data = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+
+		username = data.get('username', '')
+		password = data.get('password', '')
+
+		user = authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				login(request, user)
+				return self.create_response(request, {
+					'success': True
+				})
+			else:
+				return self.create_response(request, {
+					'success': False,
+					'reason': 'disabled',
+					}, HttpForbidden )
+		else:
+			return self.create_response(request, {
+				'success': False,
+				'reason': 'incorrect',
+				}, HttpUnauthorized )
+
+	def logout(self, request, **kwargs):
+		self.method_check(request, allowed=['get'])
+		if request.user and request.user.is_authenticated():
+			logout(request)
+			return self.create_response(request, { 'success': True })
+		else:
+			return self.create_response(request, { 'success': False }, HttpUnauthorized)
 
 class UserSignUpResource(ModelResource):
-    class Meta:
-        object_class = User
-        resource_name = 'signup'
-        fields = ['username', 'first_name', 'last_name', 'email']
-        allowed_methods = ['post']
-        include_resource_uri = False
-        authentication = Authentication()
-        authorization = Authorization()
-        queryset = User.objects.all()
+	class Meta:
+		object_class = User
+		resource_name = 'signup'
+		fields = ['username', 'first_name', 'last_name', 'email']
+		allowed_methods = ['post']
+		include_resource_uri = False
+		authentication = Authentication()
+		authorization = Authorization()
+		queryset = User.objects.all()
 
-    def obj_create(self, bundle, request=None, **kwargs):
-        try:
-            bundle = super(UserSignUpResource, self).obj_create(bundle)
-            bundle.obj.set_password(bundle.data.get('password'))
-            bundle.obj.save()
-        except IntegrityError:
-            raise BadRequest('Username already exists')
+	def obj_create(self, bundle, request=None, **kwargs):
+		try:
+			bundle = super(UserSignUpResource, self).obj_create(bundle)
+			bundle.obj.set_password(bundle.data.get('password'))
+			bundle.obj.save()
+		except IntegrityError:
+			raise BadRequest('Username already exists')
 
-        return bundle
+		return bundle
 
 class appUserResource(ModelResource):
 	user = fields.ForeignKey(UserResource, 'user')
