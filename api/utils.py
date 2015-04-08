d2836e59905f1f6c476ab8f44f990c08370baf4e
Johnny Z <JohnZadroga@gmail.com>
2015-04-08 18:45:31 -0400

    Remove dat shi

--- api/utils.py
+++ api/utils.py
@@ -4,13 +4,6 @@ MINIMUM_PASSWORD_LENGTH = 6
 REGEX_VALID_PASSWORD = (
     ## Don't allow any spaces, e.g. '\t', '\n' or whitespace etc.
     r'^(?!.*[\s])'
-    ## Check for a digit
-    '((?=.*[\d])'
-    ## Check for an uppercase letter
-    '(?=.*[A-Z])'
-    ## check for special characters. Something which is not word, digit or
-    ## space will be treated as special character
-    '(?=.*[^\w\d\s])).'
     ## Minimum 8 characters
     '{' + str(MINIMUM_PASSWORD_LENGTH) + ',}$')
  
