1465bfe710937d39d3ecc9a813e3e788b8ff4734
Kyle Venn <kvenn33@gmail.com>
2015-04-11 14:21:37 -0400

    trying new thing

--- api/api.py
+++ api/api.py
@@ -228,8 +228,9 @@ class UserProfileResource(ModelResource):
 	def obj_create(self, bundle, **kwargs):
 		return super(UserProfileResource, self).obj_create(bundle, user=bundle.request.user)
 
-	# def obj_update(self, bundle, request, **kwargs):
-	# 	return super(UserProfileResource, self).obj_update(bundle, request, **kwargs)
+	def obj_update(self, bundle, request, **kwargs):
+		kwargs["pk"] = request.user.profile.pk
+		return super(UserProfileResource, self).obj_update(bundle, request, **kwargs)
 		# identity_bundle = self.build_identity_bundle(user_bundle)
 		# IdentityResource().obj_update(identity_bundle, request)
 		# return user_bundle
