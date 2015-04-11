8f9045af21f77055a0f08bdc93c08df103023c52
Kyle Venn <kvenn33@gmail.com>
2015-04-11 13:10:51 -0400

    stuff

--- api/api.py
+++ api/api.py
@@ -123,9 +123,9 @@ class CreateUserResource(ModelResource):
 		return bundle
 
 class UserResource(ModelResource):
-	meditation_time = fields.CharField(attribute = 'profile__meditation_time')
+	# meditation_time = fields.CharField(attribute = 'profile__meditation_time')
 	exercise_day_of_week = fields.CharField(attribute = 'profile__exercise_day_of_week')
-	exercise_time = fields.CharField(attribute = 'profile__exercise_time')
+	# exercise_time = fields.CharField(attribute = 'profile__exercise_time')
  
 	class Meta:
 		authentication = Authentication()
