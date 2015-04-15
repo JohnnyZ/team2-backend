bd1ce6b40f1e798c9f28579d128df2f0ebf8cd97
Kyle Venn <kvenn33@gmail.com>
2015-04-15 00:16:48 -0400

    df

--- api/cron.py
+++ api/cron.py
@@ -1,4 +1,4 @@
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404
 
 from .models import *
@@ -8,5 +8,4 @@ logger = logging.getLogger(__name__)
 
 def dostuff():
 	for user_profile in UserProfile.objects.all():
-		logger.error(user_profile.user.email)
-		print(user_profile.user.email)
+		logger.error(user_profile.birthday)
