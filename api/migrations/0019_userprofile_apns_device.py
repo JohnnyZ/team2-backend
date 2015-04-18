4235e034685d569ada255e543804841c6e4fdce7
Kyle Venn <kvenn33@gmail.com>
2015-04-18 18:49:58 -0400

    d

--- api/migrations/0019_userprofile_apns_device.py
+++ api/migrations/0019_userprofile_apns_device.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django_extensions.db.fields
+from django.conf import settings
 
 from push_notifications import *
 
