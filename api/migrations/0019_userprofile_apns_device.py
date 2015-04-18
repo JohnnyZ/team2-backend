6916d7f969fa0be607c55aaa14a4ad46a0ca1d8a
Kyle Venn <kvenn33@gmail.com>
2015-04-18 18:40:31 -0400

    d

--- api/migrations/0019_userprofile_apns_device.py
+++ api/migrations/0019_userprofile_apns_device.py
@@ -2,13 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-# from push_notifications.models import APNSDevice
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        # ('push_notifications', '0002_auto_20150417_2029'),
+        ('push_notifications', '0002_auto_20150417_2029'),
         ('api', '0018_auto_20150415_2329'),
     ]
 
