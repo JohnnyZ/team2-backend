35197395d21e13ed831343d4bacdf15c9d14e11b
Kyle Venn <kvenn33@gmail.com>
2015-04-20 10:16:24 -0400

    patch

--- api/api.py
+++ api/api.py
@@ -231,27 +231,37 @@ class UserProfileResource(ModelResource):
 	#def authorized_read_list(self, object_list, bundle):
 	#	return object_list.filter(user=bundle.request.user).select_related()
 
-
-	# def update_in_place(self, request, original_bundle, new_data):
-
-	def put_detail(self, request, **kwargs):
+	# Serialization method that serializes the object to json before getting sent back to client
+	def dehydrate(self, bundle):
 		try:
-			self.method_check(request, allowed=['PATCH'])
-			data = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
-			# Extract the APNS Token from request
-			apns_token = data["apns_token"]
-
-			# Separate out the APNSDevice info into an object nested under the UserProfile bundle
-			# This gets sorted out by the foreign key relation in UserProfileResource
+			apns_token = bundle["apns_token"]
 			apns_device = {
 				'registration_id': apns_token
 				}
-			kwargs['apns_device'] = apns_device
-		except KeyError as missing_key:
-			raise CustomBadRequest(
-				code="missing_key",
-				message="Must provide {missing_key} when creating a user."
-						.format(missing_key=missing_key))
+			bundle["apns_device"] = apns_device
+		except KeyError:
+			pass
+ 
+		return bundle
+
+	def put_detail(self, request, **kwargs):
+		# try:
+		# 	self.method_check(request, allowed=['PATCH'])
+		# 	data = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
+		# 	# Extract the APNS Token from request
+		# 	apns_token = data["apns_token"]
+
+		# 	# Separate out the APNSDevice info into an object nested under the UserProfile bundle
+		# 	# This gets sorted out by the foreign key relation in UserProfileResource
+		# 	apns_device = {
+		# 		'registration_id': apns_token
+		# 		}
+		# 	kwargs["apns_device"] = apns_device
+		# except KeyError as missing_key:
+		# 	raise CustomBadRequest(
+		# 		code="missing_key",
+		# 		message="Must provide {missing_key} when creating a user."
+		# 				.format(missing_key=missing_key))
 
 		kwargs["pk"] = request.user.profile.pk
 		return super(UserProfileResource, self).put_detail(request, **kwargs)
