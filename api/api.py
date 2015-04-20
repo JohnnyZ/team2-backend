93bb423b39ac36c57b991fe7a6d4929da8ec1827
Kyle Venn <kvenn33@gmail.com>
2015-04-20 10:24:14 -0400

    d

--- api/api.py
+++ api/api.py
@@ -269,24 +269,24 @@ class UserProfileResource(ModelResource):
 
 
 	# TODO: Add dehydrate to this class to clean up the output of the PUT call
-	# def obj_update(self, bundle, **kwargs):
-	# 	try:
-	# 		# Extract the APNS Token from request
-	# 		apns_token = bundle.data["apns_token"]
+	def obj_update(self, bundle, **kwargs):
+		try:
+		# 	# Extract the APNS Token from request
+		# 	apns_token = bundle.data["apns_token"]
 
-	# 		# Separate out the APNSDevice info into an object nested under the UserProfile bundle
-	# 		# This gets sorted out by the foreign key relation in UserProfileResource
-	# 		apns_device = {
-	# 			'registration_id': apns_token
-	# 			}
-	# 		bundle.data['apns_device'] = apns_device
-	# 	except KeyError as missing_key:
-	# 		raise CustomBadRequest(
-	# 			code="missing_key",
-	# 			message="Must provide {missing_key} when creating a user."
-	# 					.format(missing_key=missing_key))
-	# 	kwargs["pk"] = bundle.request.user.profile.pk # TODO: is this even necessary?
-	# 	return super(UserProfileResource, self).obj_update(bundle, **kwargs)
+		# 	# Separate out the APNSDevice info into an object nested under the UserProfile bundle
+		# 	# This gets sorted out by the foreign key relation in UserProfileResource
+		# 	apns_device = {
+		# 		'registration_id': apns_token
+		# 		}
+		# 	bundle.data['apns_device'] = apns_device
+		# except KeyError as missing_key:
+		# 	raise CustomBadRequest(
+		# 		code="missing_key",
+		# 		message="Must provide {missing_key} when creating a user."
+		# 				.format(missing_key=missing_key))
+		kwargs["pk"] = bundle.request.user.profile.pk # TODO: is this even necessary?
+		return super(UserProfileResource, self).obj_update(bundle, **kwargs)
  
 	# Since there is only one user profile object, call get_detail instead
 	def get_list(self, request, **kwargs):
