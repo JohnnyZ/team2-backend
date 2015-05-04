0f870f60c4bf1cbef56db204f8638b45b455c354
Johnny Z <JohnZadroga@gmail.com>
2015-05-04 01:32:19 -0400

    assessment resource

--- api/admin.py
+++ api/admin.py
@@ -83,9 +83,9 @@ class ExerciseSessionAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
 
 
 class AssessmentResource(resources.ModelResource):
+	username = Field(attribute='userprofile__user__username')
 	class Meta:
 		model = Assessment
-		fields = ('user__username')
 
 class AssessmentAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
 	list_display = ('id', 'user_id_display', 'user', 'start_time', 'complete_time', 'created_at', 'updated_at')
